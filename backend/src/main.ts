72e1d3bf790d5fd2cf84aa515d4e99cf391362c2
jayant-ssharma <jayant0078an@gmail.com>
2026-09-18 01:43:09 +0530

    fix: allow production frontend cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,13 +3,21 @@ import { AppModule } from './app.module.ts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://job-queue-dashboard-opal-two.vercel.app',
+    ],
     credentials: true,
   });
 
-  await app.listen(3000);
-  console.log(` Server running at http://localhost:3000`);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Server running on port ${port}`);
 }
 
 bootstrap();
